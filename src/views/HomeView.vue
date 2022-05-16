8eee8230448e938b749c5b369e00367620c15171
kaurluhaaar <kaur.luhaaar@ametikool.ee>
2022-05-16 13:48:53 +0300

    imageiga koos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,27 @@
 
 <template>
   <main>
-    hello
+    <img class="logo" src="../assets/logo.png" alt="logo">
   </main>
 </template>
+
+<style>
+*{
+  padding: 0px;
+  margin: 0;
+}
+body{
+  height: 1920px;
+    background: url(src/assets/ametikool_bg.png);
+  background-color: #007ec8;
+}
+main {
+
+}
+
+.logo{
+  position: absolute;
+  left: 427px;
+  top: 581px;
+}
+</style>
