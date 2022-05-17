735a2ff2c4ecc5761bec8af4bb1818771f0dd82b
kaurluhaaar <kaur.luhaaar@ametikool.ee>
2022-05-17 10:18:21 +0300

    mess

--- src/views/BuildingView.vue
+++ src/views/BuildingView.vue
@@ -1,19 +1,45 @@
 <template>
 <RouterLink to="mess">
-    <div>
-        <h1 class="oppehoone">Mess</h1>   
-        <h1 class="Raamatukogu">Raamatukogu</h1>   
-        <h1 class="oppehoone">Juuksur</h1>   
-        <h1 class="oppehoone">IT-spetsialist</h1>   
+    <div class="oppehoone">
+        <div class="flex">
+        <span>Mess</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
+        </div>
+        <div>
+        <span>Raamatukogu</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
+        </div>
+        <div>
+        <span>Juuksur</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
+        </div>
+        <div>
+        <span>IT-spetsialist</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
+        </div>
+    </div>
+    <div class="konverentsid">
+        <span>Konverentsiruumid</span>   
+        <span>Kohvik “Kass”</span>   
+        <span>Innovatsioonilabor</span>   
+        <span>Aula</span>   
     </div>
 </RouterLink>
 </template>
 
 <style scoped>
+.arrow{
+padding-left: 20px;
+
+}
 .oppehoone{
+    display: flex;
+    flex-direction: column;
     position: absolute;
-    font-size: 40px;
+    font-size: 30px;
     top: 700px;
     left: 100px;
+    color:black;
+    gap: 10px;
+}
+.flex{
+    display: flex;
+    align-items: center;
 }
 </style>
