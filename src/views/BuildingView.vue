50670329916e5da5f6f50f8cab3eb7f08f9a41d0
kaurluhaaar <kaur.luhaaar@ametikool.ee>
2022-05-17 09:20:10 +0300

    idk

--- src/views/BuildingView.vue
+++ src/views/BuildingView.vue
@@ -1,3 +1,19 @@
 <template>
-<img src="../assets/logo.svg" alt="">
+<RouterLink to="mess">
+    <div>
+        <h1 class="oppehoone">Mess</h1>   
+        <h1 class="Raamatukogu">Raamatukogu</h1>   
+        <h1 class="oppehoone">Juuksur</h1>   
+        <h1 class="oppehoone">IT-spetsialist</h1>   
+    </div>
+</RouterLink>
 </template>
+
+<style scoped>
+.oppehoone{
+    position: absolute;
+    font-size: 40px;
+    top: 700px;
+    left: 100px;
+}
+</style>
