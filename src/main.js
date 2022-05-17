b1cd3d2b9a74f92465a3bb7f1ca00b6a497fe062
PopeFranci5 <rihardpargma@gmail.com>
2022-05-17 09:16:34 +0300

    help me

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-<<<<<<< HEAD
-=======
 import './index.css'
->>>>>>> 0bfa143750e085b50f3eaa1a3e213204ebedf5d4
 
 const app = createApp(App)
 
