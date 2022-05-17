534f5b5b3f6566b1eb8ca0ee3dc3c5e3ae8facde
PopeFranci5 <rihardpargma@gmail.com>
2022-05-17 13:22:34 +0300

    added routing to majajuht

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,9 +8,11 @@
     <div class="flex items-end h-[100%]">
         <img class="flex mb-64" src="src/images/burger.svg" alt="">
     </div>
-    <div class=" h-[100%] w-[111px] flex justify-end items-end ">
-        <div class="mb-64 flex items-center justify-center w-[111px] bg-sky-900 text-center text-[64px] rounded-l-full h-[739px] "><div class=" h-[111px] origin-center rotate-[-90deg]"><span class="text-white">MAJAJUHT</span></div></div>
-    </div>
+    <RouterLink to="buildings">
+      <div class=" h-[100%] w-[111px] flex justify-end items-end ">
+          <div class="mb-64 flex items-center justify-center w-[111px] bg-sky-900 text-center text-[64px] rounded-l-full h-[739px] "><div class=" h-[111px] origin-center rotate-[-90deg]"><span class="text-white">MAJAJUHT</span></div></div>
+      </div>
+    </RouterLink>
 </div>
   </main>
 </template>
