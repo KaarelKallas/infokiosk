3c90023923cc6cfac7fbb9f6fd48f4dd78c93346
PopeFranci5 <rihardpargma@gmail.com>
2022-05-17 13:13:26 +0300

    improved view

---
+++ src/components/MajajuhtNav.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="mt-[76px] ml-[76px]">
+    <img src="../assets/logo.png" alt="Ametikool" class="w-[72px]">
+</div>
+</template>
--- src/views/BuildingView.vue
+++ src/views/BuildingView.vue
@@ -1,60 +1,69 @@
-<template>
+<script setup>
+import MajajuhtNav from '../components/MajajuhtNav.vue';
+</script>
 
-<div class="flex font-[Signika] gap-[52px]">
-    <div class="oppehoone">
-        <div class="flex flex-col items-center gap-[29px]">
-            <img src="../assets/oppehoonelogo.svg" alt="Õppehoone">
-            <span class="text-[40px] text-[#61A60B] font-bold">ÕPPEHOONE</span>
+<template>
+<div class="font-[Signika] flex flex-col items-center">
+    <MajajuhtNav class="self-start"/>
+    
+    <h1 class="font-bold text-[96px] mb-[114px]">VALI HOONE</h1>
+    
+    <div class="flex gap-[52px]">
+        <div class="oppehoone">
+            <div class="flex flex-col items-center gap-[29px]">
+                <img src="../assets/oppehoonelogo.svg" alt="Õppehoone">
+                <span class="text-[40px] text-[#61A60B] font-bold">ÕPPEHOONE</span>
+            </div>
+            <div>
+                <RouterLink class="flex" to="mess">
+                    <span>Mess</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
+                </RouterLink>
+            </div>
+            <div class="flex">
+                <span>Raamatukogu</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
+            </div>
+            <div class="flex">
+                <span>Juuksur</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
+            </div>
+            <div class="flex">
+                <span>IT-spetsialist</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
+            </div>
         </div>
-        <div>
-            <RouterLink class="flex" to="mess">
-                <span>Mess</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
-            </RouterLink>
+        <div class="konverentsid">
+            <div class="flex flex-col items-center gap-[29px]">
+                <img src="../assets/konverentsidlogo.svg" alt="logo">
+                <span class="text-[40px] text-[#F95F05] font-bold">KONVERENTSID</span>
+            </div>
+            <div class="flex">
+                <span>Konverentsiruumid</span>   <img class="arrow" src="src/images/Noolorange.svg" alt="Nool">
+            </div>
+            <div class="flex">
+                <span>Kohvik “Kass”</span>  <img class="arrow" src="src/images/Noolorange.svg" alt="Nool">
+            </div>
+            <div class="flex">
+                <span>Innovatsioonilabor</span>  <img class="arrow" src="src/images/Noolorange.svg" alt="Nool">
+            </div>
+            <div class="flex">
+                <span>Aula</span>   <img class="arrow" src="src/images/Noolorange.svg" alt="Nool">
+            </div>
         </div>
-        <div class="flex">
-            <span>Raamatukogu</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
-        </div>
-        <div class="flex">
-            <span>Juuksur</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
-        </div>
-        <div class="flex">
-            <span>IT-spetsialist</span><img class="arrow" src="src/images/Nool.svg" alt="Nool">
-        </div>
-    </div>
-    <div class="konverentsid">
-        <div class="flex flex-col items-center gap-[29px]">
-            <img src="../assets/konverentsidlogo.svg" alt="logo">
-            <span class="text-[40px] text-[#F95F05] font-bold">KONVERENTSID</span>
-        </div>
-        <div class="flex">
-            <span>Konverentsiruumid</span>   <img class="arrow" src="src/images/Noolorange.svg" alt="Nool">
-        </div>
-        <div class="flex">
-            <span>Kohvik “Kass”</span>  <img class="arrow" src="src/images/Noolorange.svg" alt="Nool"> 
-        </div>
-        <div class="flex">
-            <span>Innovatsioonilabor</span>  <img class="arrow" src="src/images/Noolorange.svg" alt="Nool"> 
-        </div>
-        <div class="flex">
-            <span>Aula</span>   <img class="arrow" src="src/images/Noolorange.svg" alt="Nool">
-        </div>
-    </div>
-    <div class="stuudio">
-        <div class="flex flex-col items-center gap-[29px]">
-            <img src="../assets/stuudiologo.svg" alt="logo">
-            <span class="text-[40px] text-[#1B8FBE] font-bold">STUUDIO</span>
-        </div>
-        <div class="flex">
-            <span>Keraamika</span>   <img class="arrow" src="src/images/Noolblue.svg" alt="Nool">
-        </div>
-        <div class="flex">
-            <span>Maaliklass</span>  <img class="arrow" src="src/images/Noolblue.svg" alt="Nool"> 
-        </div>
-        <div class="flex">
-            <span>Dekoor</span>  <img class="arrow" src="src/images/Noolblue.svg" alt="Nool"> 
-        </div>
-        <div class="flex">
-            <span>Spaa</span>   <img class="arrow" src="src/images/Noolblue.svg" alt="Nool">
+        <div class="stuudio">
+            <div class="flex flex-col items-center gap-[29px]">
+                <img src="../assets/stuudiologo.svg" alt="logo">
+                <span class="text-[40px] text-[#1B8FBE] font-bold">STUUDIO</span>
+            </div>
+            <div class="flex">
+                <span>Keraamika</span>   <img class="arrow" src="src/images/Noolblue.svg" alt="Nool">
+            </div>
+            <div class="flex">
+                <span>Maaliklass</span>  <img class="arrow" src="src/images/Noolblue.svg" alt="Nool">
+            </div>
+            <div class="flex">
+                <span>Dekoor</span>  <img class="arrow" src="src/images/Noolblue.svg" alt="Nool">
+            </div>
+            <div class="flex">
+                <span>Spaa</span>   <img class="arrow" src="src/images/Noolblue.svg" alt="Nool">
+            </div>
         </div>
     </div>
 </div>
